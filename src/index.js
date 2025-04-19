ccb47a3f72ab9f5261b7bf6bd43e83c2c41cc3ba
Kishen <120770077+kiiigaaa@users.noreply.github.com>
2025-04-19 16:15:32 +0530

    added docker image and ci cd

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 app.use('/api/auth', authRoutes);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/health', (req, res) => res.send('OK'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
